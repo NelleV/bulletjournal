1c111757d220d46d4b7526ee3238cb2bdea720d2
Alexandra Paxton <paxton.alexandra@gmail.com>
2018-04-08 12:08:30 -0700

    Update Alex's format

--- scripts/alex.py
+++ scripts/alex.py
@@ -11,28 +11,38 @@ def alex_specific_stuff(day, date, month, year, calendar_string):
 
     # print food and social
     if day in at_work_days:
-        calendar_string = calendar_string + '\n## Food and social\n'
-        calendar_string = calendar_string + '- Morning coffee (15 min)\n'
+        calendar_string = calendar_string + '- **Food and social**\n'
+        calendar_string = calendar_string + '\t- Morning coffee (15 min)\n'
         if day != "Thursday":
-            calendar_string = calendar_string + "- Lunch (1 hour)\n"
-        calendar_string = calendar_string + '- Afternoon tea (15 min)\n'
+            calendar_string = calendar_string + "\t- Lunch (1 hour)\n"
+        calendar_string = calendar_string + '\t- Afternoon tea (15 min)\n'
+
+    # print accountability
+    if day in at_work_days:
+        calendar_string = calendar_string + "- **Accountability**\n"
+        calendar_string = calendar_string + "\t- Create to-do list (15 min)\n"
+        calendar_string = calendar_string + "\t- Update to-do list (15 min)\n"
 
     # print regular meetings
-    calendar_string = calendar_string + '\n## Meetings\n'
-    if day == "Monday":
-        calendar_string = calendar_string + ("- Weekly accountability meeting"
-                                             "with Nelle Varoquaux (1 hour)\n")
-    if day == "Wednesday":
-        calendar_string = calendar_string + "- Weekly lab meeting (1 hour)\n"
-        calendar_string = calendar_string + ("- Weekly meeting with Tom"
-                                             "Griffiths (30 min)\n")
-    if day == "Thursday":
-        calendar_string = calendar_string + "- BIDS Lunch (1 hour, 30 min)\n"
+    if day in at_work_days:
+        calendar_string = calendar_string + '- **Meetings**\n'
+        if day == "Monday":
+            calendar_string = calendar_string + ("\t - Weekly accountability "
+                                                 "meeting with Nelle Varoquaux"
+                                                 " (1 hour)\n")
+        if day == "Wednesday":
+            calendar_string = calendar_string + ("\t- Weekly lab meeting (1 "
+                                                 "hour)\n")
+            calendar_string = calendar_string + ("\t- Weekly meeting with Tom"
+                                                 " Griffiths (30 min)\n")
+        if day == "Thursday":
+            calendar_string = calendar_string + ("\t- BIDS Lunch (1 hour, 30 "
+                                                 "min)\n")
 
     # print personal things
-    calendar_string = calendar_string + '\n## Personal\n'
     if day == "Thursday":
-        calendar_string = calendar_string + "- 7pm: BAD scrimmage\n"
+        calendar_string = calendar_string + '- **Personal**\n'
+        calendar_string = calendar_string + "\t- 7pm: BAD scrimmage\n"
 
     # give us back our string
     return calendar_string
