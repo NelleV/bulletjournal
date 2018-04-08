970d30fba4a18a8348368a0c2dd50a4d39421f10
Alexandra Paxton <paxton.alexandra@gmail.com>
2018-04-08 10:57:01 -0700

    Update recurring events for Alex

--- scripts/alex.py
+++ scripts/alex.py
@@ -4,7 +4,25 @@ def alex_specific_stuff(day, date, month, year):
     """
     Alex's specific stuff
     """
-    if day == "Thursday":
-        print("- BIDS lunch: 12.30pm to 2pm")
+
+    # print food and social
+    print('## Food and social')
+    print('- Morning coffee (15 min)')
+    if day != "Thursday":
+        print("- Lunch (1 hour)")
+    print('- Afternoon tea (15 min)')
+
+    # print regular meetings
+    print('## Meetings')
     if day == "Monday":
-        print("- Alex & Nelle meeting: 11.30am to 12.30pm")
+        print("- Weekly accountability meeting with Nelle Varoquaux (1 hour)")
+    if day == "Wednesday":
+        print("- Weekly lab meeting (1 hour)")
+        print("- Weekly meeting with Tom Griffiths (30 min)")
+    if day == "Thursday":
+        print("- BIDS Lunch (1 hour, 30 min)")
+
+    # print personal things
+    print('## Personal')
+    if day == "Thursday":
+        print("- 7pm: BAD scrimmage")
