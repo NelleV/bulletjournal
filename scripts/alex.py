edb4fc7354472f843535db99435b73a73c0aab1c
Alexandra Paxton <paxton.alexandra@gmail.com>
2018-04-08 11:42:12 -0700

    Specify on-location versus remote working days for Alex

--- scripts/alex.py
+++ scripts/alex.py
@@ -3,12 +3,19 @@ def alex_specific_stuff(day, date, month, year, calendar_string):
     Alex's specific stuff
     """
 
+    # specify work days
+    at_work_days = ['Monday',
+                    'Tuesday',
+                    'Wednesday',
+                    'Thursday']
+
     # print food and social
-    calendar_string = calendar_string + '\n## Food and social\n'
-    calendar_string = calendar_string + '- Morning coffee (15 min)\n'
-    if day != "Thursday":
-        calendar_string = calendar_string + "- Lunch (1 hour)\n"
-    calendar_string = calendar_string + '- Afternoon tea (15 min)\n'
+    if day in at_work_days:
+        calendar_string = calendar_string + '\n## Food and social\n'
+        calendar_string = calendar_string + '- Morning coffee (15 min)\n'
+        if day != "Thursday":
+            calendar_string = calendar_string + "- Lunch (1 hour)\n"
+        calendar_string = calendar_string + '- Afternoon tea (15 min)\n'
 
     # print regular meetings
     calendar_string = calendar_string + '\n## Meetings\n'
