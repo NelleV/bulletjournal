c891022b6d1ee404e8b014eb989439af6caeb73b
Alexandra Paxton <paxton.alexandra@gmail.com>
2018-04-30 12:14:03 -0700

    Add weekly overviews to daily list

--- scripts/alex.py
+++ scripts/alex.py
@@ -13,19 +13,62 @@ def alex_daily_specific(day, date, month, year, calendar_string):
     # specify all work days
     all_work_days = at_work_days + ['Sunday']
 
+    # initialize the calendar string
+    calendar_string = ''
+
+    # create a weekly summary section
+    if day == 'Monday':
+
+        # create headers
+        weekly_summary_header = ('# Weekly to-do list for week of ' +
+                                 month + ' ' + date + ', ' + year + '\n')
+        two_week_goal_header = '\n## Two-week goal: \n'
+        weekly_goal_header = ('\n## Three goals for the week:\n' +
+                              '1. ...\n' +
+                              '1. ...\n' +
+                              '1. ...\n')
+
+        # create weekly overview
+        weekly_overview_header = ('\n## Overview of things I need to do ' +
+                                  'this week\n')
+        weekly_overview = ''
+        for day in at_work_days:
+            weekly_overview = '- **' + day + '** (hours)\n'
+
+        # create breakdown by project
+        breakdown_header = '\n## Breakdown by project\n'
+        breakdown_table = ('| Project | Day | Time |\n' +
+                           '| ------- | --- | ---- |\n')
+
+        # create scratchpad for things I need to do
+        scratchpad_header = '\n## Other things I need to do\n'
+
+        # assemble it
+        calendar_string = (calendar_string +
+                           weekly_summary_header +
+                           two_week_goal_header +
+                           weekly_goal_header +
+                           weekly_overview_header +
+                           weekly_overview +
+                           breakdown_header +
+                           breakdown_table +
+                           scratchpad_header +
+                           '\n***\n\n')
+
     # create header for work days
     if day in all_work_days:
 
         # add goal header
         goal_subheader = ('### Primary foci: \n\n')
-        calendar_string = calendar_string + goal_subheader
 
         # print table formatting header
         table_header = ("| Project | Activity | Anticipated time | Total time"
                         " |\n"
                         "|---------|----------|------------------|------------"
                         "|\n")
-        calendar_string = calendar_string + table_header
+
+        # assemble it
+        calendar_string = calendar_string + goal_subheader + table_header
 
     # print food and social
     if day in at_work_days:
